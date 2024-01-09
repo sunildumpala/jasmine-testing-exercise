e957bd055ca93d5eef6d149558bb5a704bbe053e
sunildumpala <dumpala@gmail.com>
2024-01-09 10:43:03 -0800

    loan calculator now functional

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -26,15 +26,38 @@ function setupIntialValues() {
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let values = getCurrentUIValues();
+  console.log(values);
+  let monthly = calculateMonthlyPayment(values);
+  updateMonthly(monthly);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  console.log("in the calculateMonthlyPayment function");
+  let monthly;
+  let monthlyraw;
+  let principal = values.amount;
+  let term = values.years*12;
+  let interest = values.rate/1200;
+  console.log("in the calculateMonthlyPayment function: ", principal, term, interest);
+  //monthly = Math.round((((principal * interest)/(1-((1+interest)**(-term))))*100)/100) ;
+  monthlyraw = (principal * interest)/(1-((1+interest)**(-term)));
+  console.log("Monthly raw comes out to: ", monthlyraw);
+  //monthly = (Math.round(monthlyraw * 100))/100;
+  monthly = parseFloat(monthlyraw).toFixed(2);
+  console.log("Monthly comes out to: ", monthly);
+  
+  return monthly;
+
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  let monthlydiv = document.getElementById("monthly-payment");
+  console.log(monthlydiv);
+  monthlydiv.textContent = monthly;
 }
