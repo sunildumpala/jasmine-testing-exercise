fe45ad181824f4bcb8f55ff051b1ad56457eea4f
sunildumpala <dumpala@gmail.com>
2024-01-09 10:52:50 -0800

    updated calculator tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,29 @@
 
 it('should calculate the monthly rate correctly', function () {
   // ...
+  let values= {
+    amount: 1000,
+    rate: 6,
+    years: 10
+  };
+  //values.amount = 1000;
+  //values.rate = 6;
+  //values.years = 10;
+  expect(calculateMonthlyPayment(values)).toEqual("11.10");
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  let values= {
+    amount: 1000,
+    rate: 6,
+    years: 10
+  };
+  //values.amount = 1000;
+  //values.rate = 6;
+  //values.years = 10;
+  expect(calculateMonthlyPayment(values)).toEqual("11.10");
 });
 
 /// etc
